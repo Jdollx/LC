8256200d1eb1ebf589a0c1d169ffc2f68da0857a
Jdollx <jenniferdolldev@gmail.com>
2025-02-17 19:55:59 -0500

    best time to buy and sell stock with pointers

--- Two_Pointers/Best_Time_to_Buy_and_Sell_Stock.py
+++ Two_Pointers/Best_Time_to_Buy_and_Sell_Stock.py
@@ -33,3 +33,32 @@ class Solution:
                 max_profit = profit
 
         return max_profit
+
+
+# two pointers methodclass Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # initiate two pointers, one will determine lowest day and then
+        # move right to get highest day (buy and sell)
+        left = 0
+        right = 1
+
+        # initiate profit
+        max_profit = 0
+        
+        # iterate through while right is still in bounds of arr
+        while right < len(prices):
+            # make sure left stays less than right
+            if prices[left] < prices[right]:
+                # formula for finding current profit
+                profit = prices[right] - prices[left]
+
+                # update maxprofit if new max profit found
+                max_profit = max(max_profit, profit)
+            
+        # if no max profit found, update pointers
+            else:
+                # means we found a lower buy price
+                left = right
+            right += 1
+        return max_profit
+
